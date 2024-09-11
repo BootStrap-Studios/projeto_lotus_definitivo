165390718751035ef81331011915006af723bdd9
Vinicius Pimentel Monteiro <vinipm4@gmail.com>
2024-09-10 22:00:22 -0300

    é isso

--- Assets/Scripts/Cameras/ThirdPersonCam.cs
+++ Assets/Scripts/Cameras/ThirdPersonCam.cs
@@ -1,3 +1,4 @@
+using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,11 +7,27 @@ public class ThirdPersonCam : MonoBehaviour
 {
     [Header("Variavéis/Referências")]
     [SerializeField] private Transform orientation;
+    [SerializeField] private Transform combatLookAt;
     [SerializeField] private Transform player;
     [SerializeField] private Transform playerObj;
     [SerializeField] private Rigidbody rb;
 
     [SerializeField] private float rotationSpeed;
+    [SerializeField] private CameraStyle currentStyle;
+
+    [Header("Cameras")]
+    [SerializeField] private CinemachineFreeLook basicCamera; 
+    [SerializeField] private CinemachineFreeLook combatCamera; 
+    [SerializeField] private CinemachineFreeLook topDownCamera; 
+    
+
+    public enum CameraStyle
+    {
+        Basic,
+        Combat,
+        TopDown
+    }
+
 
     void Start()
     {
@@ -20,7 +37,16 @@ public class ThirdPersonCam : MonoBehaviour
 
     void Update()
     {
-        CalculandoOrientacao();
+        MyInputs();
+        //CalculandoOrientacao();
+        QualCamera();
+    }
+
+    private void MyInputs()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1)) SwitchCameraStyle(CameraStyle.Basic);
+        if (Input.GetKeyDown(KeyCode.Alpha2)) SwitchCameraStyle(CameraStyle.Combat);
+        if (Input.GetKeyDown(KeyCode.Alpha3)) SwitchCameraStyle(CameraStyle.TopDown);
     }
 
     private void CalculandoOrientacao()
@@ -29,15 +55,45 @@ public class ThirdPersonCam : MonoBehaviour
         Vector3 viewDir = player.position - new Vector3(transform.position.x, player.position.y, transform.position.z);
         orientation.forward = viewDir.normalized;
 
-        //Rotate player object
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
-        Vector3 inputDir = orientation.forward * verticalInput + orientation.right * horizontalInput;
+    }
 
-        if(inputDir != Vector3.zero)
+    private void QualCamera()
+    {
+        if(currentStyle == CameraStyle.Basic || currentStyle == CameraStyle.TopDown)
         {
-            playerObj.forward = Vector3.Slerp(playerObj.forward, inputDir.normalized, Time.deltaTime * rotationSpeed);
+            Vector3 viewDir = player.position - new Vector3(transform.position.x, player.position.y, transform.position.z);
+            orientation.forward = viewDir.normalized;
+
+            //Rotate player object
+            float horizontalInput = Input.GetAxis("Horizontal");
+            float verticalInput = Input.GetAxis("Vertical");
+            Vector3 inputDir = orientation.forward * verticalInput + orientation.right * horizontalInput;
+
+            if (inputDir != Vector3.zero)
+            {
+                playerObj.forward = Vector3.Slerp(playerObj.forward, inputDir.normalized, Time.deltaTime * rotationSpeed);
+            }
+        } else if(currentStyle == CameraStyle.Combat)
+        {
+            //Rotate orientation
+            Vector3 viewDirCombat = combatLookAt.position - new Vector3(transform.position.x, combatLookAt.position.y, transform.position.z);
+            orientation.forward = viewDirCombat.normalized;
+
+            playerObj.forward = viewDirCombat.normalized;
         }
+    }
+
+    private void SwitchCameraStyle(CameraStyle newStyle)
+    {
+        basicCamera.Priority = 0;
+        combatCamera.Priority = 0;
+        topDownCamera.Priority = 0;
+
+        if (newStyle == CameraStyle.Basic) basicCamera.Priority = 10;
+        if (newStyle == CameraStyle.Combat) combatCamera.Priority = 10;
+        if (newStyle == CameraStyle.TopDown) topDownCamera.Priority = 10;
+
+        currentStyle = newStyle;
 
     }
 }
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -5,9 +5,22 @@ using UnityEngine.Windows;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [Header("Ground Check")]
+    [SerializeField] private float playerHeight;
+    [SerializeField] private LayerMask whatIsGround;
+    bool grounded;
+
     [Header("Movement")]
     [SerializeField] private float moveSpeed;
     [SerializeField] private Transform orientation;
+    [SerializeField] private float groundDrag;
+
+    [Header("Jump")]
+    [SerializeField] private float jumpForce;
+    [SerializeField] private float jumpCooldown;
+    [SerializeField] private float airMultiplier;
+    bool readyToJump = true;
+
 
     float horizontalInput;
     float verticalInput;
@@ -25,11 +38,18 @@ public class PlayerMovement : MonoBehaviour
     private void Update()
     {
         MyInput();
+        GroundCheck();
+        
+
+        
+        
     }
 
     private void FixedUpdate()
     {
         MovePlayer();
+        SpeedControl();
+        HandleDrag();
     }
 
     private void MyInput()
@@ -37,6 +57,33 @@ public class PlayerMovement : MonoBehaviour
         horizontalInput = UnityEngine.Input.GetAxisRaw("Horizontal");
         verticalInput = UnityEngine.Input.GetAxisRaw("Vertical");
 
+        if(UnityEngine.Input.GetKey(KeyCode.Space) && readyToJump && grounded)
+        {
+            readyToJump = false;
+
+            Jump();
+
+            Invoke(nameof(ResetJump), jumpCooldown);
+        }
+    }
+
+    private void GroundCheck()
+    {
+        //ground check
+        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, whatIsGround);
+
+        
+    }
+    
+    private void HandleDrag()
+    {
+        if(grounded)
+        {
+            rb.drag = groundDrag;
+        } else
+        {
+            rb.drag = 0f;
+        }
     }
 
     private void MovePlayer()
@@ -44,8 +91,40 @@ public class PlayerMovement : MonoBehaviour
         //Calculate move direction
         moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
 
-        rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
+        if(grounded)
+        {
+            rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
+
+        } else if(!grounded)
+        {
+            rb.AddForce(moveDirection.normalized * moveSpeed * 10f * airMultiplier, ForceMode.Force);
+        }
+        
 
     }
 
+    private void SpeedControl()
+    {
+        Vector3 flatVelocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+        if(flatVelocity.magnitude > moveSpeed)
+        {
+            Vector3 limitedVelocity = flatVelocity.normalized * moveSpeed;
+            rb.velocity = new Vector3(limitedVelocity.x, rb.velocity.y, limitedVelocity.z);
+        }
+    }
+
+    private void Jump()
+    {
+        
+        //Reset velocity
+        rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+        rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+    }
+
+    private void ResetJump()
+    {
+        readyToJump = true;
+    }
 }
