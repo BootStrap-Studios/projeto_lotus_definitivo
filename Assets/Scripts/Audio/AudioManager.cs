0f605a5a5b1c8533f0bab57cb3d235178b426b7e
Vinicius Pimentel Monteiro <vinipm4@gmail.com>
2024-11-15 14:57:41 -0300

    vai gellard

--- Assets/Scripts/Audio/AudioManager.cs
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -8,6 +8,9 @@ public class AudioManager : MonoBehaviour
 {
     public static AudioManager instance { get; private set; }
 
+    private List<EventInstance> eventInstances;
+    private List<StudioEventEmitter> eventEmitters;
+
     private void Awake()
     {
         if(instance != null)
@@ -15,6 +18,9 @@ public class AudioManager : MonoBehaviour
             Destroy(gameObject);
         }
         instance = this;
+
+        eventInstances = new List<EventInstance>();
+        eventEmitters = new List<StudioEventEmitter>();
     }
 
     public void PlayOneShot(EventReference sound, Vector3 worldPos)
@@ -25,7 +31,36 @@ public class AudioManager : MonoBehaviour
     public EventInstance CreateEventInstance(EventReference eventReference)
     {
         EventInstance eventInstance = RuntimeManager.CreateInstance(eventReference);
+        eventInstances.Add(eventInstance);
+
         return eventInstance;
+    }
+
+    public StudioEventEmitter InitializeEventEmitter(EventReference eventReference, GameObject emiterGameObject)
+    {
+        StudioEventEmitter emitter = emiterGameObject.GetComponent<StudioEventEmitter>();
+        emitter.EventReference = eventReference;
+        eventEmitters.Add(emitter);
+        return (emitter);
+    }
+
+    private void CleanUp()
+    {
+        foreach(EventInstance eventInstance in eventInstances)
+        {
+            eventInstance.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
+            eventInstance.release();
+        }
+
+        foreach(StudioEventEmitter emitter in eventEmitters)
+        {
+            emitter.Stop();
+        }
+    }
+
+    private void OnDestroy()
+    {
+        CleanUp();
     }
 
 }
