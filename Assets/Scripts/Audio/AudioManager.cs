805f555b20182fb88a54a949778649c7d9298501
Vinicius Pimentel Monteiro <vinipm4@gmail.com>
2024-11-16 00:05:08 -0300

    menu de volume e import do ink

--- Assets/Scripts/Audio/AudioManager.cs
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -11,6 +11,25 @@ public class AudioManager : MonoBehaviour
     private List<EventInstance> eventInstances;
     private List<StudioEventEmitter> eventEmitters;
 
+    [Header("Volume")]
+
+    [Range(0, 1)]
+    public float masterVolume = 1;
+
+    [Range(0, 1)]
+    public float musicVolume = 1;
+
+    [Range(0, 1)]
+    public float ambienceVolume = 1;
+
+    [Range(0, 1)]
+    public float sfxVolume = 1;
+
+    private Bus masterBus;
+    private Bus musicBus;
+    private Bus ambienceBus;
+    private Bus sfxBus;
+
     private void Awake()
     {
         if(instance != null)
@@ -21,6 +40,22 @@ public class AudioManager : MonoBehaviour
 
         eventInstances = new List<EventInstance>();
         eventEmitters = new List<StudioEventEmitter>();
+
+        masterBus = RuntimeManager.GetBus("bus:/");
+
+        musicBus = RuntimeManager.GetBus("bus:/Music");
+
+        ambienceBus = RuntimeManager.GetBus("bus:/Ambience");
+
+        sfxBus = RuntimeManager.GetBus("bus:/SFX");
+    }
+
+    private void Update()
+    {
+        masterBus.setVolume(masterVolume);
+        musicBus.setVolume(musicVolume);
+        ambienceBus.setVolume(ambienceVolume);
+        sfxBus.setVolume(sfxVolume);
     }
 
     public void PlayOneShot(EventReference sound, Vector3 worldPos)
